f9c9be34ec105b57e2714f12b965297137252e8e
wangpeijian <519955464@qq.com>
2018-06-22 18:26:45 +0800

    初始化

---
+++ src/main/java/com/org/serviceconfig/ServiceConfigApplication.java
@@ -0,0 +1,12 @@
+package com.org.serviceconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceConfigApplication.class, args);
+    }
+}
